52f57643543bed424fdefd4a2f7910cc81f0d49a
Robin Tollisen <robin.tollisen@gmail.com>
2013-02-03 19:16:11 +0100

    Added buttons to GUI.

---
+++ AdaptiveTD/AdaptiveTD/AdaptiveTD/GUI.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace AdaptiveTD
+{
+    public struct TowerStats
+    {
+        Texture2D towerTexture;
+        public Texture2D TowerTexture
+        {
+            get { return towerTexture; }
+            set { towerTexture = value; }
+        }
+        Texture2D missileTexture;
+
+        public Texture2D MissileTexture
+        {
+            get { return missileTexture; }
+            set { missileTexture = value; }
+        }
+        float towerReloadTime;
+
+        public float TowerReloadTime
+        {
+            get { return towerReloadTime; }
+            set { towerReloadTime = value; }
+        }
+        int damage;
+        public int Damage
+        {
+            get { return damage; }
+            set { damage = value; }
+        }
+        public TowerStats(Texture2D towerTexture, Texture2D missileTexture, float towerReloadTime, int damage)
+        {
+            this.towerTexture = towerTexture;
+            this.missileTexture = missileTexture;
+            this.towerReloadTime = towerReloadTime;
+            this.damage = damage;
+        }
+    }
+    class GUI
+    {
+        Dictionary<GUIButton, TowerStats> towerButtons = new Dictionary<GUIButton, TowerStats>();
+        Dictionary<string, TowerStats> towers = new Dictionary<string, TowerStats>();
+
+        Vector2 position;
+        Vector2 startingDrawPos = new Vector2(20, 20);
+        int buttonPadding = 10;
+
+        Texture2D GUITexture;
+
+        public GUI(Vector2 position, Dictionary<string, TowerStats> towers, Texture2D GUITexture)
+        {
+            this.position = position;
+            this.towers = towers;
+            foreach (KeyValuePair<string, TowerStats> tower in towers)
+            {
+                towerButtons.Add(new GUIButton(tower.Value.TowerTexture, new Vector2(startingDrawPos.X + position.X + towerButtons.Count * buttonPadding, position.Y + startingDrawPos.Y)), tower.Value);
+                startingDrawPos.X += tower.Value.TowerTexture.Width;
+            }
+            this.GUITexture = GUITexture;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            Vector2 hitPosition = new Vector2(Mouse.GetState().X, Mouse.GetState().Y);
+            foreach (KeyValuePair<GUIButton, TowerStats> button in towerButtons)
+            {
+                if (button.Key.ButtonClicked((int)hitPosition.X, (int)hitPosition.Y))
+                    button.Key.Color = Color.Red;
+                else
+                    button.Key.Color = Color.White;
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(GUITexture, position, Color.White);
+
+            foreach (KeyValuePair<GUIButton, TowerStats> tower in towerButtons)
+            {
+                tower.Key.Draw(spriteBatch);
+            }
+        }
+    }
+}
---
+++ AdaptiveTD/AdaptiveTD/AdaptiveTD/GUIButton.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace AdaptiveTD
+{
+    class GUIButton
+    {
+        Texture2D texture;
+        Vector2 position;
+        Color color;
+        public Color Color
+        {
+            set { color = value; }
+        }
+
+        public GUIButton(Texture2D texture, Vector2 position)
+        {
+            this.texture = texture;
+            this.position = position;
+            color = Color.White;
+        }
+
+        public bool ButtonClicked(int x, int y)
+        {
+            if (x >= position.X && x <= position.X + texture.Width && y >= position.Y && y <= position.Y + texture.Height)
+                return true;
+            else
+                return false;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, position, color);
+        }
+    }
+}
