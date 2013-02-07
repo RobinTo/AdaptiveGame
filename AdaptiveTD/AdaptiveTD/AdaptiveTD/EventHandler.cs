b2dc981e7356b7dc4172e38d31e28d18e92f791b
Robin Tollisen <robint09@student.uia.no>
2013-02-07 13:23:00 +0100

    Added missing files.

---
+++ AdaptiveTD/AdaptiveTD/AdaptiveTD/EventHandler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdaptiveTD
+{
+    class EventHandler
+    {
+
+        List<Event> events = new List<Event>();
+
+        List<Event> queuedEvents = new List<Event>();
+
+        public List<Event> Events
+        {
+            get
+            {
+                return events;
+            }
+        }
+
+        public void NewRound()
+        {
+            events.Clear();
+            for (int i = 0; i < queuedEvents.Count; i++)
+                events.Add(queuedEvents[i]);
+            queuedEvents.Clear();
+        }
+
+        public void QueueEvent(Event e)
+        {
+            queuedEvents.Add(e);
+        }
+    }
+}
