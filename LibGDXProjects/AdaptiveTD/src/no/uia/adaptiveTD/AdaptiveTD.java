42ab1e39b776625161b87cbab6dc946d02e94793
Godspark <park533@gmail.com>
2013-02-21 15:38:02 +0100

    formatting

--- LibGDXProjects/AdaptiveTD/src/no/uia/adaptiveTD/AdaptiveTD.java
+++ LibGDXProjects/AdaptiveTD/src/no/uia/adaptiveTD/AdaptiveTD.java
@@ -18,36 +18,36 @@ public class AdaptiveTD implements ApplicationListener {
 	private Texture texture;
 	private Sprite sprite;
 	private BitmapFont font;
-	
+
 	boolean unlockFrames = false;
 	boolean onlyUpdates = false;
 	float w, h;
 	CharSequence UPS = "0";
 	CharSequence FPS = "0";
-	
+
 	@Override
-	public void create() {		
+	public void create() {
 		w = Gdx.graphics.getWidth();
 		h = Gdx.graphics.getHeight();
-		
-		if(unlockFrames)
+
+		if (unlockFrames)
 			Gdx.graphics.setVSync(false);
-		
-		camera = new OrthographicCamera(1, h/w);
+
+		camera = new OrthographicCamera(1, h / w);
 		camera.setToOrtho(false, w, h);
 		batch = new SpriteBatch();
 
 		font = new BitmapFont();
-		
+
 		texture = new Texture(Gdx.files.internal("data/libgdx.png"));
 		texture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
-		
+
 		TextureRegion region = new TextureRegion(texture, 0, 0, 512, 275);
-		
+
 		sprite = new Sprite(region);
 		sprite.setSize(0.9f, 0.9f * sprite.getHeight() / sprite.getWidth());
-		sprite.setOrigin(sprite.getWidth()/2, sprite.getHeight()/2);
-		sprite.setPosition(-sprite.getWidth()/2, -sprite.getHeight()/2);
+		sprite.setOrigin(sprite.getWidth() / 2, sprite.getHeight() / 2);
+		sprite.setPosition(-sprite.getWidth() / 2, -sprite.getHeight() / 2);
 	}
 
 	@Override
@@ -62,21 +62,21 @@ public class AdaptiveTD implements ApplicationListener {
 	int frameC = 0;
 	int frameT = 0;
 	double frameTimer = 0;
+
 	@Override
-	public void render() {		
+	public void render() {
 
 		float gameTime = Gdx.graphics.getDeltaTime();
 		frameTimer += gameTime;
 		timer -= frameTimer;
-		Update((float)(gameTime-frameTimer));
+		update((float) (gameTime - frameTimer));
 		updateC++;
-		if(!onlyUpdates)
-			Draw();
+		if (!onlyUpdates)
+			draw();
 		frameC++;
 		frameTimer = 0;
-			
-		if(timer <= 0.0)
-		{
+
+		if (timer <= 0.0) {
 			updateT = updateC;
 			frameT = frameC;
 			updateC = 0;
@@ -86,23 +86,22 @@ public class AdaptiveTD implements ApplicationListener {
 		FPS = Integer.toString(frameT);
 		UPS = Integer.toString(updateT);
 	}
-	private void Update(float gameTime)
-	{
+
+	private void update(float gameTime) {
 	}
-	
-	private void Draw()
-	{
+
+	private void draw() {
 
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
 		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
 		sprite.draw(batch);
-		font.draw(batch, FPS, 10, h-10);
-		font.draw(batch, UPS, 10, h-20);
+		font.draw(batch, FPS, 10, h - 10);
+		font.draw(batch, UPS, 10, h - 20);
 		batch.end();
 	}
-	
+
 	@Override
 	public void resize(int width, int height) {
 	}
