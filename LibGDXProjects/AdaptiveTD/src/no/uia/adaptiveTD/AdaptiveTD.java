b1c6ad1df35bac613ec24e9a453c3623840cafce
Robin Tollisen <robint09@student.uia.no>
2013-02-21 15:34:49 +0100

    Added basic game loop.

--- LibGDXProjects/AdaptiveTD/src/no/uia/adaptiveTD/AdaptiveTD.java
+++ LibGDXProjects/AdaptiveTD/src/no/uia/adaptiveTD/AdaptiveTD.java
@@ -2,10 +2,12 @@ package no.uia.adaptiveTD;
 
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.Texture.TextureFilter;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
@@ -15,14 +17,27 @@ public class AdaptiveTD implements ApplicationListener {
 	private SpriteBatch batch;
 	private Texture texture;
 	private Sprite sprite;
+	private BitmapFont font;
+	
+	boolean unlockFrames = false;
+	boolean onlyUpdates = false;
+	float w, h;
+	CharSequence UPS = "0";
+	CharSequence FPS = "0";
 	
 	@Override
 	public void create() {		
-		float w = Gdx.graphics.getWidth();
-		float h = Gdx.graphics.getHeight();
+		w = Gdx.graphics.getWidth();
+		h = Gdx.graphics.getHeight();
+		
+		if(unlockFrames)
+			Gdx.graphics.setVSync(false);
 		
 		camera = new OrthographicCamera(1, h/w);
+		camera.setToOrtho(false, w, h);
 		batch = new SpriteBatch();
+
+		font = new BitmapFont();
 		
 		texture = new Texture(Gdx.files.internal("data/libgdx.png"));
 		texture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
@@ -41,17 +56,53 @@ public class AdaptiveTD implements ApplicationListener {
 		texture.dispose();
 	}
 
+	int updateC = 0;
+	int updateT = 0;
+	double timer = 1.0;
+	int frameC = 0;
+	int frameT = 0;
+	double frameTimer = 0;
 	@Override
 	public void render() {		
+
+		float gameTime = Gdx.graphics.getDeltaTime();
+		frameTimer += gameTime;
+		timer -= frameTimer;
+		Update((float)(gameTime-frameTimer));
+		updateC++;
+		if(!onlyUpdates)
+			Draw();
+		frameC++;
+		frameTimer = 0;
+			
+		if(timer <= 0.0)
+		{
+			updateT = updateC;
+			frameT = frameC;
+			updateC = 0;
+			frameC = 0;
+			timer = 1.0;
+		}
+		FPS = Integer.toString(frameT);
+		UPS = Integer.toString(updateT);
+	}
+	private void Update(float gameTime)
+	{
+	}
+	
+	private void Draw()
+	{
+
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
-		
 		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
 		sprite.draw(batch);
+		font.draw(batch, FPS, 10, h-10);
+		font.draw(batch, UPS, 10, h-20);
 		batch.end();
 	}
-
+	
 	@Override
 	public void resize(int width, int height) {
 	}
