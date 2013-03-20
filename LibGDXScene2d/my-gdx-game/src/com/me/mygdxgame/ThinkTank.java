6f099dfab4897889f7a1ec69e730235243c0916b
Robin Tollisen <robint09@student.uia.no>
2013-03-20 12:46:41 +0100

    Changes

--- LibGDXScene2d/my-gdx-game/src/com/me/mygdxgame/ThinkTank.java
+++ LibGDXScene2d/my-gdx-game/src/com/me/mygdxgame/ThinkTank.java
@@ -6,26 +6,31 @@ import java.util.List;
 
 public class ThinkTank {
 
-	HashMap<String, Float> parameters = new HashMap<String, Float>();
+	HashMap<Integer, HashMap<String, Float>> measurements = new HashMap<Integer, HashMap<String, Float>>();
 	
 	public void UpdateParameters(float totalTime, int gold, int lives, List<Tower> towers, List<Event> events, HashMap<String, TowerStats> availableTowers){
-		
-		parameters.put("totalTime", totalTime);
-		parameters.put("goldPerLife", (float)(gold/lives));
-		parameters.put("APM", parameters.get("APM") + events.size()); // Do Events/totalTime when done
-		parameters.put("lives", (float)lives);
-		
-		float variety = 0;
-		List<String> types = new ArrayList<String>();
-		for(Tower t : towers)
+
+		if(Math.floor(totalTime)%10 == 0 && !measurements.containsKey(Math.floor(totalTime)))
 		{
-			if(!types.contains(t.towerStats.type))
+			HashMap<String, Float> parameters = new HashMap<String, Float>();
+			parameters.put("totalTime", totalTime);
+			parameters.put("goldPerLife", (float)(gold/lives));
+			parameters.put("APM", parameters.get("APM") + events.size()); // Do Events/totalTime when done
+			parameters.put("lives", (float)lives);
+			
+			float variety = 0;
+			List<String> types = new ArrayList<String>();
+			for(Tower t : towers)
 			{
-				variety++;
-				types.add(t.towerStats.type);
+				if(!types.contains(t.towerStats.type))
+				{
+					variety++;
+					types.add(t.towerStats.type);
+				}
 			}
+			variety = variety/availableTowers.size();
+			parameters.put("variety", variety);
+			measurements.put((int)Math.floor(totalTime), parameters);
 		}
-		variety = variety/availableTowers.size();
-		parameters.put("variety", variety);
 	}
 }
