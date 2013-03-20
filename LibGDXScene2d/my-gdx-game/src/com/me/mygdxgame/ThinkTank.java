8fc523459b4e86c4d5966e442c10cf4012840c3d
Robin Tollisen <robint09@student.uia.no>
2013-03-20 12:52:57 +0100

    Added file.

--- LibGDXScene2d/my-gdx-game/src/com/me/mygdxgame/ThinkTank.java
+++ LibGDXScene2d/my-gdx-game/src/com/me/mygdxgame/ThinkTank.java
@@ -11,12 +11,12 @@ public class ThinkTank {
 
 	HashMap<Integer, HashMap<String, Float>> measurements = new HashMap<Integer, HashMap<String, Float>>();
 	int actionCounter = 0;
-	int timePerTick = 10;
+	int timeBetweenMeasurements = 10;
 	
 	public void updateParameters(float totalTime, int gold, int lives, List<Tower> towers, List<Event> events, HashMap<String, TowerStats> availableTowers){
 
 		actionCounter += events.size();
-		if(Math.floor(totalTime)%timePerTick == 0 && !measurements.containsKey(Math.floor(totalTime)))
+		if(Math.floor(totalTime)%timeBetweenMeasurements == 0 && !measurements.containsKey(Math.floor(totalTime)))
 		{
 			HashMap<String, Float> parameters = new HashMap<String, Float>();
 			parameters.put("totalTime", totalTime);
