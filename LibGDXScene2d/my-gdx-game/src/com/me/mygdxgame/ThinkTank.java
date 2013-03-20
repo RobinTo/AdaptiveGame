2499d95f78d72cf6cfb607e320103dff8f3b30ab
Godspark <park533@gmail.com>
2013-03-20 11:58:58 +0100

    ThinkTank update

--- LibGDXScene2d/my-gdx-game/src/com/me/mygdxgame/ThinkTank.java
+++ LibGDXScene2d/my-gdx-game/src/com/me/mygdxgame/ThinkTank.java
@@ -2,7 +2,10 @@ package com.me.mygdxgame;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+
+import com.badlogic.gdx.files.FileHandle;
 
 public class ThinkTank {
 
@@ -12,7 +15,10 @@ public class ThinkTank {
 		
 		parameters.put("totalTime", totalTime);
 		parameters.put("goldPerLife", (float)(gold/lives));
-		parameters.put("APM", parameters.get("APM") + events.size()); // Do Events/totalTime when done
+		if (parameters.containsKey("APM"))
+			parameters.put("APM", parameters.get("APM") + events.size()); // Do Events/totalTime when done
+		else
+			parameters.put("APM", (float) events.size()); 
 		parameters.put("lives", (float)lives);
 		
 		float variety = 0;
@@ -27,5 +33,28 @@ public class ThinkTank {
 		}
 		variety = variety/availableTowers.size();
 		parameters.put("variety", variety);
+	}
+	
+	public void WriteParameters(FileHandle handle)
+	{
+		Iterator<String> keyIterator = parameters.keySet().iterator();
+		handle.writeString("Parameter List\r\n\r\n", false);
+		while(keyIterator.hasNext())
+		{
+			String s = keyIterator.next();
+			Float parameter = parameters.get(s);
+			
+				try
+				{
+					handle.writeString(s +":"+parameter+":\r\n", true);
+					System.out.println(s +":"+parameter+"\n");
+				}
+				catch(Exception e)
+				{
+					System.out.println(s +":"+parameter+"\n");
+					System.out.println(e.toString());
+				}
+		}
+		System.out.println("Saved Replay successfully");
 	}
 }
