50af8a937a3d6b3f77d2140023a76261f145a89f
Robin Tollisen <robin.tollisen@gmail.com>
2013-04-09 03:55:57 +0200

    Added console and a decent wave.

---
+++ LibGDXScene2d/my-gdx-game/src/com/me/mygdxgame/ConsoleActor.java
@@ -0,0 +1,12 @@
+package com.me.mygdxgame;
+
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
+public class ConsoleActor {
+
+	public ConsoleActor(Sprite background, BitmapFont font)
+	{
+		
+	}
+}
